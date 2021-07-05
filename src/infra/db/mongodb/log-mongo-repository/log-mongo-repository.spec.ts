93a354446b1c7bbd268b0c2b2c6cdf77ce1d6f68
Caroline Corrêa <31678882+carolineccorrea@users.noreply.github.com>
2021-07-04 23:57:46 -0300

    feat: add account mongodb repository

---
+++ src/infra/db/mongodb/log-mongo-repository/log-mongo-repository.spec.ts
@@ -0,0 +1,32 @@
+import { Collection } from 'mongodb'
+import faker from 'faker'
+import { MongoHelper } from '../mongo-helper/mongo-helper'
+import { LogMongoRepository } from './log-mongo-repository'
+
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
+
+let errorCollection: Collection
+
+describe('LogMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    errorCollection = await MongoHelper.getCollection('errors')
+    await errorCollection.deleteMany({})
+  })
+
+  test('Should create an error log on success', async () => {
+    const sut = makeSut()
+    await sut.logError(faker.random.words())
+    const count = await errorCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
